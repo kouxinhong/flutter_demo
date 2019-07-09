805e963971f5e3c60f65f65cd3bac8b9d5cbae2b
kouzi <kou_xinhong@sina.cn>
2019-07-09 22:16:22 +0800

    独立appBar

--- demo1/lib/main.dart
+++ demo1/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import './bar/AppBarWidget.dart'  as AppBarWidget;
-import 'dart:convert';
-import 'package:flutter/services.dart';
+import './bar/my_app_bar.dart' as MyAppBar;
 import './service/BannerSevice.dart' as BannerSevice;
-import './model/HomeBannerModel.dart';
 import './model/HomeBannerItemModel.dart';
 
 void main() {
@@ -14,48 +11,17 @@ class HomeAppBar extends StatefulWidget {
   @override
   _HomeAppBarState createState() => _HomeAppBarState();
 }
+
 class _HomeAppBarState extends State<HomeAppBar> {
-  List<Widget> _actions;
+  // Choice _selectedChoice = choices[0];
+  // void _select(Choice choice) {
+  //   setState(() {
+  //     // Causes the app to rebuild with the new _selectedChoice.
+  //     _selectedChoice = choice;
+  //   });
+  // }
 
-  List<Widget> _getAction() {
-    if (null != _actions) {
-      return _actions;
-    }
-    _actions = List<Widget>();
-    for (int i = 0; i < 2; i++) {
-      IconButton iconButton = IconButton(
-        icon: choices[i].icon,
-        onPressed: () {
-          _select(choices[i]);
-        },
-      );
-      _actions.add(iconButton);
-    }
-    PopupMenuButton popupMenuButton = PopupMenuButton<Choice>(
-        icon: choices[2].icon,
-        onSelected: _select,
-        itemBuilder: (BuildContext context) {
-          return choices.skip(3).map<PopupMenuItem<Choice>>((Choice choice) {
-            return PopupMenuItem(
-                value: choice,
-                child: ListTile(
-                  leading: choice.icon,
-                  title:
-                      Text(choice.title, style: TextStyle(color: Colors.white)),
-                ));
-          }).toList();
-        });
-    _actions.add(popupMenuButton);
-    return _actions;
-  }
-
-  Choice _selectedChoice = choices[0];
-  void _select(Choice choice) {
-    setState(() {
-      // Causes the app to rebuild with the new _selectedChoice.
-      _selectedChoice = choice;
-    });
-  }
+  List<Widget> _actions = MyAppBar.actions;
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +32,7 @@ class _HomeAppBarState extends State<HomeAppBar> {
         home: Scaffold(
             appBar: AppBar(
               title: const Text('Flutter'),
-              actions: AppBarWidget,
+              actions: _actions,
             ),
             body: HomeListView()));
   }
@@ -123,7 +89,7 @@ class _HomeListViewState extends State<HomeListView> {
                       Divider(height: 1, color: Colors.brown),
                       Text(
                         values[index].title,
-                        style: TextStyle(height: 1, fontSize: 33),
+                        style: TextStyle(height: 1, fontSize: 20),
                         textAlign: TextAlign.center,
                       )
                     ],
@@ -131,19 +97,3 @@ class _HomeListViewState extends State<HomeListView> {
             }));
   }
 }
-
-class Choice {
-  Choice({this.icon, this.title});
-
-  final Icon icon;
-  final String title;
-}
-
-List<Choice> choices = <Choice>[
-  Choice(title: 'Search', icon: Icon(Icons.search, color: Colors.white)),
-  Choice(title: 'box', icon: Icon(Icons.add_box, color: Colors.white)),
-  Choice(title: 'setting', icon: Icon(Icons.settings, color: Colors.white)),
-  Choice(title: 'setting', icon: Icon(Icons.settings, color: Colors.white)),
-  Choice(title: 'feedback', icon: Icon(Icons.feedback, color: Colors.white)),
-  Choice(title: 'report', icon: Icon(Icons.report, color: Colors.white)),
-];
