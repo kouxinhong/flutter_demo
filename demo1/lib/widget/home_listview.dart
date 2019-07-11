abff3e4e9b60fee133db043c9cf6c7c634151a3e
kouzi <kou_xinhong@sina.cn>
2019-07-11 22:24:54 +0800

    delete test code

---
+++ demo1/lib/widget/home_listview.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../service/BannerSevice.dart' as BannerSevice;
+import '../model/HomeBannerItemModel.dart';
+
+class HomeListView extends StatefulWidget {
+  @override
+  _HomeListViewState createState() => _HomeListViewState();
+}
+
+class _HomeListViewState extends State<HomeListView> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: BannerSevice.decodeBanner(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Text('loading...');
+            default:
+              if (snapshot.hasError)
+                return Text('Error: ${snapshot.error}');
+              else
+                return createContainer(context, snapshot);
+          }
+        });
+  }
+
+  Widget createContainer(BuildContext context, AsyncSnapshot snapshot) {
+    List<HomeBannerItemModel> values = snapshot.data;
+    return Container(
+        color: Colors.grey,
+        child: ListView.builder(
+            scrollDirection: Axis.vertical,
+//            padding: EdgeInsets.all(10.0),
+            shrinkWrap: true,
+            itemCount: values.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  color: Colors.white,
+                  elevation: 10,
+                  margin: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                  child: Column(
+                    children: <Widget>[
+                      ClipRRect(
+//                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(15)),
+                        child: Image.network(values[index].imagePath),
+                      ),
+                      Divider(height: 1, color: Colors.brown),
+                      Text(
+                        values[index].title,
+                        style: TextStyle(height: 1, fontSize: 20),
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ));
+            }));
+  }
+}
