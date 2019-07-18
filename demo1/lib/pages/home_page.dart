eeaadae9116bc43da4bf2ee7f22aeea5c927fe22
kxh <kouxinhong@apkpure.net>
2019-07-18 18:37:24 +0800

    格式化代码

--- demo1/lib/pages/home_page.dart
+++ demo1/lib/pages/home_page.dart
@@ -9,31 +9,18 @@ class HomePage extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
           title: Text('彻底解析Android缓存机制——LruCache'),
-//          subtitle: RichText(text: TextSpan(
-//            text: '彻底',
-//            style: TextStyle(color: Colors.red,backgroundColor: Colors.red,),
-//            children: <TextSpan>[
-//              TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
-//              TextSpan(text: ' world!'),
-//            ]
-//          )),
-          subtitle: Row(
-              children: <Widget>[
-                Chip(label: Text('置顶'),labelPadding: EdgeInsets.zero,shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(2))
-                )),
-                Chip(label: Text('问答')),
-                Chip(label: Text('dada')),
-              ]),
+          subtitle: Row(children: <Widget>[
+            Chip(
+                label: Text('置顶'),
+                labelPadding: EdgeInsets.zero,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(2)))),
+            Chip(label: Text('问答')),
+            Chip(label: Text('dada')),
+          ]),
           trailing: Icon(Icons.favorite, color: Colors.red),
         );
       },
     );
   }
 }
-
-//Padding(
-//padding: EdgeInsets.all(4),
-//child: RichText(
-//text: TextSpan(
-//text: '置顶', style: DefaultTextStyle.of(context).style))),
